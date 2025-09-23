cb7b0ab7bc90a136ce65ad3552dcbdd7b8484747
alice <alicemarinhogms@gmail.com>
2025-09-23 18:38:33 -0300

    Revert "add TP 02"
    This reverts commit 8397a1e9225d2dd6ec22b32b0fd535a284f55a2a.
    
    git push origin main

--- sistweb-container-bl/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace sistweb_container_bl.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
