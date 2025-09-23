8397a1e9225d2dd6ec22b32b0fd535a284f55a2a
alice <alicemarinhogms@gmail.com>
2025-09-23 18:35:02 -0300

    add TP 02

---
+++ sistweb-container-bl/Migrations/20250923180746_InitialCreate.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace sistweb_container_bl.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BL",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    numero = table.Column<string>(type: "text", nullable: false),
+                    consignee = table.Column<string>(type: "text", nullable: false),
+                    navio = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BL", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Containers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    numero = table.Column<char>(type: "character(1)", nullable: false),
+                    tipo = table.Column<string>(type: "text", nullable: false),
+                    tamanho = table.Column<string>(type: "text", nullable: false),
+                    Id_bl = table.Column<int>(type: "integer", nullable: false),
+                    BLId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Containers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Containers_BL_BLId",
+                        column: x => x.BLId,
+                        principalTable: "BL",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Containers_BLId",
+                table: "Containers",
+                column: "BLId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Containers");
+
+            migrationBuilder.DropTable(
+                name: "BL");
+        }
+    }
+}
---
+++ sistweb-container-bl/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sistweb_container_bl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
